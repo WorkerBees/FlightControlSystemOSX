12abb2e3d7a0cb31050ae94c4110423d2a4d57fb
Craig R. Hughes <craig.git@rungie.com>
2014-08-26 14:26:55 -0700

    Express interest in some messages and for now just log them to console.  We should really have some way of creating listener/handlers and having them register/unregister as appropriate.

--- FlightControlSystemOSX/AppDelegate.m
+++ FlightControlSystemOSX/AppDelegate.m
@@ -8,17 +8,41 @@
 
 #import "AppDelegate.h"
 
+#import "FCSMAVLinkMessage.h"
+
 @implementation AppDelegate
             
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     NSLog(@"App delegate created and finished launching");
     self.connectionLinkManager = [[FCSConnectionLinkManager alloc] init];
+
+    NSLog(@"Registering for notifications on certain message types");
+    // Broadcast that we received this message
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_HEARTBEAT] object:nil];
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_STATUSTEXT] object:nil];
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_SYS_STATUS] object:nil];
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_SYSTEM_TIME] object:nil];
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_MEMINFO] object:nil];
+
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_MISSION_REQUEST_LIST] object:nil];
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_MISSION_COUNT] object:nil];
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_MISSION_REQUEST] object:nil];
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_MISSION_ITEM] object:nil];
+    [nc addObserver:self selector:@selector(logMAVLinkMessage:) name:[FCSMAVLinkMessage nameForMessageID:MAVLINK_MSG_ID_MISSION_ACK] object:nil];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification
 {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
     self.connectionLinkManager = nil;
+}
+
+- (void)logMAVLinkMessage:(NSNotification *)notification
+{
+    FCSMAVLinkMessage *msg = [notification.userInfo objectForKey:@"message"];
+    NSLog(@"Received: %@", msg);
 }
 
 @end
