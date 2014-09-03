f045b0c38604024d77a44e6b7ea281cdb3acd84d
Craig R. Hughes <craig.git@rungie.com>
2014-09-03 11:31:05 -0700

    Include waypoint sequence number in title and in annotation image

--- FlightControlSystemOSX/FCSAnnotation.m
+++ FlightControlSystemOSX/FCSAnnotation.m
@@ -8,6 +8,12 @@
 
 #import "FCSAnnotation.h"
 
+@interface FCSAnnotation ()
+
+@property NSString *titleBase;
+
+@end
+
 @implementation FCSAnnotation
 
 @synthesize coordinate=_coordinate;
@@ -24,33 +30,82 @@
     CGPoint handle_offset;
     switch(self.mission_item.command)
     {
-        case FCSMAVCMDType_NAV_LAND:     handle_offset = CGPointMake(-21.25, -29); break;
-        case FCSMAVCMDType_NAV_WAYPOINT: handle_offset = CGPointMake(0, -30); break;
-        case FCSMAVCMDType_NAV_TAKEOFF:  handle_offset = CGPointMake(17.5, -25); break;
+        case FCSMAVCMDType_NAV_LAND:
+            handle_offset = CGPointMake(-21.25, -29);
+            break;
+        case FCSMAVCMDType_NAV_WAYPOINT:
+            handle_offset = CGPointMake(0, -30);
+            break;
+        case FCSMAVCMDType_NAV_TAKEOFF:
+            handle_offset = CGPointMake(17.5, -25);
+            break;
         default:
             @throw [NSException exceptionWithName:@"Bad mission item"
                                            reason:@"You should not have a mission item of that type here!"
                                          userInfo:@{@"mission_item":self.mission_item}];
     }
-    view.image = [NSImage imageNamed:self.title];
+
+    // Search for cached image with waypoint number already scribbled on it
+    NSImage *img = [NSImage imageNamed:self.title];
+    if(!img)
+    {
+        img = [[NSImage imageNamed:self.titleBase] copy];
+        // We need to overlay on the image the waypoint number
+        [img lockFocus];
+        NSString *waypointNumber = [NSString stringWithFormat:@"%u", self.mission_item.sequenceNumber];
+        NSMutableParagraphStyle * paragraphStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+        NSFont *font = [NSFont systemFontOfSize:24];
+        NSShadow *shadow = [[NSShadow alloc] init];
+        shadow.shadowColor = [NSColor blackColor];
+        shadow.shadowBlurRadius = 3.0;
+        shadow.shadowOffset = CGSizeMake(0, 0);
+
+        NSDictionary * attributes = @{ NSParagraphStyleAttributeName: paragraphStyle,
+                                       NSFontAttributeName: font,
+                                       NSForegroundColorAttributeName: [NSColor whiteColor],
+                                       NSShadowAttributeName: shadow,
+                                       };
+
+        NSRect textOverlayDrawRect;
+        switch(self.mission_item.command)
+        {
+            case FCSMAVCMDType_NAV_LAND:
+                textOverlayDrawRect = NSMakeRect(0, 2,
+                                                 img.size.width, font.pointSize);
+                paragraphStyle.alignment = NSLeftTextAlignment;
+                break;
+            case FCSMAVCMDType_NAV_WAYPOINT:
+                textOverlayDrawRect = NSMakeRect(0, img.size.height/2,
+                                                 img.size.width, font.pointSize);
+                paragraphStyle.alignment = NSCenterTextAlignment;
+                break;
+            case FCSMAVCMDType_NAV_TAKEOFF:
+                textOverlayDrawRect = NSMakeRect(0, 2,
+                                                 img.size.width, font.pointSize);
+                paragraphStyle.alignment = NSRightTextAlignment;
+                break;
+            default:
+                @throw [NSException exceptionWithName:@"Bad mission item"
+                                               reason:@"You should not have a mission item of that type here!"
+                                             userInfo:@{@"mission_item":self.mission_item}];
+        }
+
+        [waypointNumber drawInRect:textOverlayDrawRect withAttributes:attributes];
+        [img unlockFocus];
+
+        // Now cache it
+        img.name = self.title;
+    }
+
+    view.image = img;
     view.centerOffset = handle_offset;
     view.canShowCallout = YES;
-
-    NSImageView *calloutImageView = [[NSImageView alloc] init];
-    calloutImageView.image = [NSImage imageNamed:self.title];
-    view.leftCalloutAccessoryView = calloutImageView;
     view.draggable = YES;
 }
 
 - (NSString *)title
 {
-    switch(self.mission_item.command)
-    {
-        case FCSMAVCMDType_NAV_LAND: return @"Land";
-        case FCSMAVCMDType_NAV_WAYPOINT: return @"Waypoint";
-        case FCSMAVCMDType_NAV_TAKEOFF: return @"Takeoff";
-        default: return @"Waypoint";
-    }
+    return [NSString stringWithFormat:@"%u: %@", self.mission_item.sequenceNumber, self.titleBase];
 }
 
 - (NSString *)subtitle
@@ -63,6 +118,14 @@
     self = [self init];
 
     self.mission_item = mission_item;
+    switch(self.mission_item.command)
+    {
+        case FCSMAVCMDType_NAV_LAND: self.titleBase = @"Land"; break;
+        case FCSMAVCMDType_NAV_TAKEOFF: self.titleBase = @"Takeoff"; break;
+        case FCSMAVCMDType_NAV_WAYPOINT:
+        default: self.titleBase = @"Waypoint";
+    }
+
     _coordinate = CLLocationCoordinate2DMake(mission_item.x, mission_item.y);
 
     return self;
